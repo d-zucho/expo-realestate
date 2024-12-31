c1fab3ce9204aa62a5949a663aaf9b7baea39c82
D_Zucho <danny.nico.dev@outlook.com>
2024-12-30 22:34:07 -0800

    add initial splash screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,9 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
+      <Text className='font-bold text-3xl my-10 font-rubik '>
+        Welcome to ReState
+      </Text>
       <Link href={'/sign-in'}>Sign In</Link>
       <Link href={'/explore'}>Explore</Link>
       <Link href={'/profile'}>Profile</Link>
